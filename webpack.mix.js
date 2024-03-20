f599819204fa5b64bd0c03e6bb0157f7cbd1201f
MikusR <mikus.rakis@gmail.com>
2024-03-20 10:35:51 +0200

    initial

---
+++ webpack.mix.js
@@ -0,0 +1,19 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        //
+    ]);
+mix.js('src/app.js', 'dist').vue();
+mix.js('src/app.js', 'dist').vue({ version: 3 });
