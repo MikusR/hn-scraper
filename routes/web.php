c1816c533852bc9ac6ff150d04f4f13ae9035623
MikusR <mikus.rakis@gmail.com>
2024-04-29 19:02:03 +0300

    fix delete

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'api/v0', 'middleware' => ['auth']], function () {
     Route::get('/index', [LinkController::class, 'index'])->name('index');
-    Route::delete('/delete-link/{link}', [LinkController::class, 'index'])->name('delete-link');
+    Route::delete('/delete-link/{link}', [LinkController::class, 'deleteLink'])->name('delete-link');
     Route::get('/user', [LinkController::class, 'checkUser'])->name('check');
 });
 
