c2aa52a5325b38eb1b8020321db26a9d2e3735ee
Mikus Rakis <mikus.rakis@gmail.com>
2025-04-06 16:09:44 +0300

    debug

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\LinkController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
-
 
 Route::group(['prefix' => 'api/v0', 'middleware' => ['auth']], function () {
     Route::get('/index', [LinkController::class, 'index'])->name('index');
@@ -17,18 +15,13 @@ Route::get('/test', function () {
     echo 'Current directory: ' . getcwd() . "<br>";
     echo 'PHP version: ' . phpversion() . "<br>";
     echo base_path('app') . "<br>";
-    echo storage_path() . "<br>";
-    $filePath = 'persistent-db/file.txt'; // Adjust the path as needed
-if (Storage::exists($filePath)) {
-    $contents = Storage::get($filePath);
-    echo $contents. "<br>";
-} else {
-    echo "File does not exist.";
-}
+
+
 $dbtest = base_path('app')."/persistent-db/db.txt";
+file_put_contents($dbtest, "test");
 echo "dbtest= ".$dbtest . "<br>";
 echo file_get_contents($dbtest);
-Storage::put($filePath, "write test");
+
 });
 
 Route::fallback([AppController::class, 'main']);
