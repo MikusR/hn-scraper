9043b1d30b5d4a1dad6a0f3d004589fc6f1d0787
Mikus Rakis <mikus.rakis@gmail.com>
2025-04-06 16:01:00 +0300

    Enhance test route to display environment details and file operations

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\LinkController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 
 Route::group(['prefix' => 'api/v0', 'middleware' => ['auth']], function () {
@@ -12,7 +13,22 @@ Route::group(['prefix' => 'api/v0', 'middleware' => ['auth']], function () {
 });
 
 Route::get('/test', function () {
-    echo get_current_user();
+    echo get_current_user() . "<br>";
+    echo 'Current directory: ' . getcwd() . "<br>";
+    echo 'PHP version: ' . phpversion() . "<br>";
+    echo base_path('app') . "<br>";
+    echo storage_path() . "<br>";
+    $filePath = 'persistent-db/file.txt'; // Adjust the path as needed
+if (Storage::exists($filePath)) {
+    $contents = Storage::get($filePath);
+    echo $contents. "<br>";
+} else {
+    echo "File does not exist.";
+}
+$dbtest = base_path('app')."/persistent-db/db.txt";
+echo "dbtest= ".$dbtest . "<br>";
+echo file_get_contents($dbtest);
+Storage::put($filePath, "write test");
 });
 
 Route::fallback([AppController::class, 'main']);
