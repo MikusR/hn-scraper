c6f59d4614e294517d0ae6643461bebdbf597d56
Mikus Rakis <mikus.rakis@gmail.com>
2026-05-30 20:34:46 +0300

    Migrate scheduled tasks to Laravel 11 way. From app/Console/Hernel.php to routes/console.php

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command("scrape:fromapi top")->everyThreeHours();
+Schedule::command("scrape:fromapi best")->everyTwoHours();
+Schedule::command("scrape:fromapi new")->HourlyAt(32);
